78ef96b405e6d838c5ed6af7c61fb163825b4387
FranciscoRoges09 <rogesfrancisco695@gmail.com>
2025-07-31 06:21:46 -0300

    Adicionando popup de parabéns e atualizando página de upsell

---
+++ upssel1/script.js
@@ -0,0 +1,5 @@
+function adquirirOferta() {
+  alert('Oferta adicionada ao seu pedido com sucesso!');
+  // Aqui você pode adicionar redirecionamento para próxima etapa
+  // window.location.href = 'https://checkout.desafiodeagosto.online/VCCL1O8SC4I8';
+}
